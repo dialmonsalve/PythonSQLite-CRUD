5eeb8a2e6013ea6794a801a51748e80303b99c43
Diego Alejandro <diales1981@gmail.com>
2022-08-08 11:55:39 -0500

    Organizando carpetas

---
+++ src/catalogo-peliculas.py
@@ -0,0 +1,21 @@
+#-------dialmonsalve@dialmonsalve.onmicrosoft.com------
+""" This is the module wich includes the main exec window """
+# ###########################################################################
+# 	Project: App for training												#
+# 	Create by: Diego Alejandro Monsalve	E									# 
+#	Start date: 29/04/2022													#
+#	End date: 																#
+# 	Copryght: 2022															#
+# ###########################################################################
+import tkinter as tk
+
+def main():
+	root = tk.Tk()
+	root.title('Catalogo de peliculas')
+	root.iconbitmap('src/img/2Movies_37443.ico')
+
+	root.mainloop()
+
+
+if __name__ =='__main__':
+	main()
